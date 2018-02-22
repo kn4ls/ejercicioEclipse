b1c00073165588c5bb202c4e69cfef58b0309711
kn4ls <jordicanalsros@gmail.com>
2018-02-22 18:19:35 +0100

    actualitzem el missatge finalitzador

--- Finally/src/com/viewnext/Main.java
+++ Finally/src/com/viewnext/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		try {
 			cA.error();
 			System.out.println("No peta!!!");
-			System.out.println("Sortim!!");
+			System.out.println("Sortim ADEUU!!");
 		} catch (Exception ex) {
 			System.out.println("He petado 2!");
 			throw new Exception("Subo la ex", ex);
